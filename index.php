5826ffae655c1333118c988e4e3b1f54736aab08
AJ <a.gouda200@gmail.com>
2020-11-24 12:48:34 +0200

    commit

--- index.php
+++ index.php
@@ -3,16 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
     <title>backend</title>
 </head>
 <body>
-<button onclick="document.getElementById('myInput').value = ''">Clear input field</button>
-    <form action="" method="POST" autocomplete="off">
-    <label for="username">usernamE</label>
-    <input type="text" name="username" value=' ' id="myInput" autocomplete="off" placeholder="name" >
+<div>
+<form action="" method="POST" autocomplete="off">
+    <label for="username">username</label>
+    <input type="text" name="username" value='' id="myInput" placeholder="name" >
+    <br>
     <label for="password">password</label>
-    <input type="password" name="password" value="">
-    <input type="submit" name="submit">
+    <input type="password" name="password" placeholder="password" value="">
+    <br>
+    <button type="submit" name="submit"> submit </button>
     </form>
+    </div>
 </body>
 </html>
